eee2005e4760542ff1fdd755b8e1ade6773b2b7e
junhan-kim <kjune1236@gmail.com>
2021-10-04 06:56:09 +0900

    Add candle function

---
+++ market.py
@@ -0,0 +1,22 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+server_url = os.getenv('UPBIT_OPEN_API_SERVER_URL')
+
+
+def get_candle(market_id, unit, count, min_unit=None):
+    querystring = {"market": market_id, "count": count}
+
+    headers = {"Accept": "application/json"}
+
+    if unit == 'minutes':
+        response = requests.request(
+            "GET", f"{server_url}/v1/candles/{unit}/{min_unit}", headers=headers, params=querystring)
+    else:
+        response = requests.request(
+            "GET", f"{server_url}/v1/candles/{unit}", headers=headers, params=querystring)
+
+    return response.json()
